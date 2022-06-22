0b4e43b692abddf4454d67364c3aaff7595498fe
«aleksandr» <gertman84@mail.ru>
2022-06-22 23:42:33 +0300

    6 урок РАНДОМНЫЙ

---
+++ urok06/Program.cs
@@ -0,0 +1,5 @@
+﻿int NumberA = new Random().Next(1,10); //1.1.3....9
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1,10);
+Console.WriteLine(NumberB);
+Console.WriteLine(NumberA + NumberB);
