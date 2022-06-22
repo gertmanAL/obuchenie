0a998e79cf9e6da689968b0c9cc6474904bc8568
«aleksandr» <gertman84@mail.ru>
2022-06-23 00:06:42 +0300

    7 урок ПРИВЕТСТВИЕ ИМЕНИ

---
+++ Urok07/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ведите ваше имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "зайка")
+{
+    Console.WriteLine("ура");
+}
+else
+{
+    Console.WriteLine("привет ");
+    Console.WriteLine(username);
+}    
