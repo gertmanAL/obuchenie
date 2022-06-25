6c61c65d33c8f142b6954f81ab47d9f2d9817fbb
«aleksandr» <gertman84@mail.ru>
2022-06-25 15:51:33 +0300

    возведеник в квадрат

---
+++ urok10/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number = Convert.ToInt32(Console.ReadLine());
+int q = number * number;
+Console.WriteLine ( number + " -> " + q);
