e0b02767d72b83ad0a31bf28cdef2ac249f5ee95
«aleksandr» <gertman84@mail.ru>
2022-06-25 16:18:19 +0300

    проверка первого числа квадратом второго

---
+++ urok11/Program.cs
@@ -0,0 +1,17 @@
+﻿// напишите програму которая на вход принимает 2 числа и проверяет является первое числоквадратом второго
+
+Console.Write("input number ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("input number ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+int g = number_2 * number_2;
+if (number_1 == g)
+
+    Console.Write("a = " + number_1 + "b=" + number_2 + "->" + "yes");
+
+else
+{
+    Console.Write("a = " + number_1 + "b=" + number_2 + "->" + "no");
+}
