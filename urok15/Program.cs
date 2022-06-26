1d8c2ffb609dac556230ee15a819a1dd71f9e5d7
«aleksandr» <gertman84@mail.ru>
2022-06-26 22:59:31 +0300

    вывод чисел от -н до н

---
+++ urok15/Program.cs
@@ -0,0 +1,10 @@
+﻿// вывод чисел от -Н до Н
+Console.WriteLine("ВВЕДИТЕ ЧИСЛО");
+int number = Convert.ToInt32(Console.ReadLine());
+int FN = -number;
+while(FN <= number)
+{
+    if(FN <= number)
+    Console.Write(FN + ".");
+    FN++;
+}  
