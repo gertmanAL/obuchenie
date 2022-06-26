13e97783302e6e7c81ab086308831177e400d87a
«aleksandr» <gertman84@mail.ru>
2022-06-26 22:07:17 +0300

    вывод из 3 чисел мах

---
+++ urok14/Program.cs
@@ -0,0 +1,16 @@
+﻿// программа на вход принимает 3 числа и выдает мах этих чисел
+Console.WriteLine("ведите число");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("ведите число");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("ведите число");
+int number_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number_1;
+
+if(number_2 > max) max = number_2;
+if(number_3 > max) max = number_3;
+
+Console.Write(" максимальное число" + max); 
