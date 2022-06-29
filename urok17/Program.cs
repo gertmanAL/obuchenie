bbfe4c4b31a54b9f0aa500bf08a16af5ac7e4b6e
«aleksandr» <gertman84@mail.ru>
2022-06-29 20:44:41 +0300

    рамдомное число из отрезка

---
+++ urok17/Program.cs
@@ -0,0 +1,17 @@
+﻿// одномерный массив из N элементов равный fait
+
+int[] array = { 4345,3455,3455,32,334,5,5,443};
+int N = array.Length;
+int find = 32;
+int index = 0;
+while(index < N)
+{
+    if (array[index] == find)
+    
+        Console.WriteLine(index);
+        index++;
+    else if ( Console.WriteLine("ненашел ничего"));
+}   
+
+Console.WriteLine("нашел");
+
