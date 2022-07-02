0d9a1271a7067b1881013960ea79f36a930ec59b
«aleksandr» <gertman84@mail.ru>
2022-07-02 15:29:00 +0300

    уменьшение числа до 100

---
+++ urok20 dz/Program.cs
@@ -0,0 +1,24 @@
+﻿//вывод 3числа и цифры
+void Div(){
+Console.WriteLine("ВВЕДИТЕ ЧИСЛО");
+int a1 = Convert.ToInt32(Console.ReadLine());
+int g =999;
+if(a1 > g)
+ do 
+ {
+   a1 = a1/10;
+}while(a1 > g);
+
+ 
+ if (a1 < g)
+ {
+    int posle2 = a1 % 10;
+    Console.WriteLine(a1 + "->" + posle2);
+ }
+
+
+  
+if(a1<100)
+Console.WriteLine(a1 + "даное число 2-х значное и 3-й цифри в нем нет");
+}
+
